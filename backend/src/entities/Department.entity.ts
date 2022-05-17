3653704c4803ae7615dcba54f28ee2a472fe7521
k-tkm <kjohlkjou89@gmail.com>
2022-05-17 10:27:33 +0900

    create departmens table

---
+++ backend/src/entities/Department.entity.ts
@@ -0,0 +1,30 @@
+import { DateTime } from 'luxon';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { User } from './User.entity';
+import { UserDepartment } from './UserDepartment.entity';
+
+@Entity('departments')
+export class Department {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 200, unique: true })
+  name: string;
+
+  @CreateDateColumn({ type: 'datetime', name: 'created_at' })
+  createdAt: DateTime;
+
+  @UpdateDateColumn({ type: 'datetime', name: 'updated_at' })
+  updatedAt: DateTime;
+
+  @DeleteDateColumn({ type: 'datetime', name: 'deleted_at', nullable: true })
+  deletedAt: DateTime;
+}
