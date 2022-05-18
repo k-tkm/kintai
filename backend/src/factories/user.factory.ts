1d7516792c169b392315c9dfbb17c556e5c4b17b
k-tkm <kjohlkjou89@gmail.com>
2022-05-19 08:49:24 +0900

    Fixed values to be put in createdAt and updatedAt in user.factory.ts

--- backend/src/factories/user.factory.ts
+++ backend/src/factories/user.factory.ts
@@ -2,12 +2,14 @@ import { define } from 'typeorm-seeding';
 import { faker } from '@faker-js/faker';
 import { User } from 'src/entities/User.entity';
 
+const now = new Date();
+
 define(User, () => {
   const user = new User();
   user.lastName = faker.name.lastName();
   user.firstName = faker.name.firstName();
   user.email = faker.internet.email();
-  user.createdAt = faker.date.soon();
-  user.updatedAt = faker.date.soon();
+  user.createdAt = now;
+  user.updatedAt = now;
   return user;
 });
