3e4583a7af5434debfe9ebc084af61c76e088004
k-tkm <kjohlkjou89@gmail.com>
2022-05-24 08:47:21 +0900

    Fix to use environment variables in app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,11 +7,11 @@ import { AppService } from './app.service';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'db',
-      port: 3306,
-      username: 'develop',
-      password: 'password',
-      database: 'kintai',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [__dirname + '/entities/*.entity{.ts,.js}'],
       synchronize: false,
     }),
