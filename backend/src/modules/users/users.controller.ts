ac741f6a93722c2df40bd2718a63d6db19b67fc3
k-tkm <kjohlkjou89@gmail.com>
2022-05-25 11:20:58 +0900

    Create an endpoint that issues a jwt token when logged in

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -3,6 +3,11 @@ import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/entities/User.entity';
 import { UsersService } from './users.service';
 
+export type userDataType = {
+  email: string;
+  lastName: string;
+  firstName: string;
+};
 @Controller('users')
 export class UsersController {
   constructor(
@@ -12,5 +17,9 @@ export class UsersController {
   @Get()
   async getUsers(): Promise<User[]> {
     return await this.usersService.getUsers();
+  }
+  @Post()
+  async saveUser(@Body() userData: userDataType): Promise<{ token: string }> {
+    return await this.usersService.saveUser(userData);
   }
 }
--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -1,16 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/User.entity';
 import { Repository } from 'typeorm';
+import { userDataType } from './users.controller';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    private jwtService: JwtService,
   ) {}
 
   getUsers(): Promise<User[]> {
     return this.usersRepository.find();
+  }
+
+  async saveUser(userData: userDataType): Promise<{ token: string }> {
+    const { lastName, firstName, email } = userData;
+    const existUser = await this.usersRepository.findOne({
+      email: email,
+    });
+    if (existUser) {
+      this.usersRepository.save({
+        ...existUser,
+        lastName: lastName,
+        firstName: firstName,
+      });
+    } else {
+      this.usersRepository.create(userData);
+    }
+
+    const payload = { username: lastName + firstName };
+    const token = this.jwtService.sign(payload);
+    return { token: token };
   }
 }
