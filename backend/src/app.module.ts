423dd7ef025aca96e6fd5904a426db9ff58ccea7
k-tkm <kjohlkjou89@gmail.com>
2022-05-26 10:04:04 +0900

    Fixed a bug that environment variables could not be read

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
 import { UsersController } from './modules/users/users.controller';
+import { UserModule } from './modules/users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
