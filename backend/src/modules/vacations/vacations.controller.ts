8f80c6cbaa42bd58245f90f0b8420417a5a470ee
k-tkm <kjohlkjou89@gmail.com>
2022-06-06 11:29:18 +0900

    Create an endpoint to retrieve vacations

--- backend/src/modules/vacations/vacations.controller.ts
+++ backend/src/modules/vacations/vacations.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Vacation } from 'src/entities/Vacation.entity';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { getVacationsQueryDto } from './Dto/getVacationsQuery.Dto';
+import { VacationsService } from './vacations.service';
 
 @Controller('vacations')
-export class VacationsController {}
+export class VacationsController {
+  constructor(private vacationsService: VacationsService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  get(@Query() query: getVacationsQueryDto): Promise<Vacation[]> {
+    return this.vacationsService.get(query);
+  }
+}
