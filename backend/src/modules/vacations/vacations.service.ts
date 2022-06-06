c86cc9b41d989b91bd6c14cc9226f4be4636f7f6
k-tkm <kjohlkjou89@gmail.com>
2022-06-06 11:29:18 +0900

    Create an endpoint to retrieve vacations

--- backend/src/modules/vacations/vacations.service.ts
+++ backend/src/modules/vacations/vacations.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Vacation } from 'src/entities/Vacation.entity';
+import { Repository } from 'typeorm';
+import { getVacationsQueryDto } from './Dto/getVacationsQuery.Dto';
 
 @Injectable()
-export class VacationsService {}
+export class VacationsService {
+  constructor(
+    @InjectRepository(Vacation)
+    private vacationsRepository: Repository<Vacation>,
+  ) {}
+
+  async getVacations(query: getVacationsQueryDto): Promise<Vacation[]> {
+    const { startDate, endDate, user_id } = query;
+    const vacations = await this.vacationsRepository
+      .createQueryBuilder('vacations')
+      .leftJoinAndSelect('user', 'u')
+      .where('vacations.date between :startDate and :endDate', {
+        startDate: startDate,
+        endDate: endDate,
+      })
+      .andWhere('u.id = :id', { id: user_id })
+      .getMany();
+
+    return vacations;
+  }
+}
