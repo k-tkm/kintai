9f3666389e95706bf886b450553ac6854918005f
k-tkm <kjohlkjou89@gmail.com>
2022-06-07 10:50:13 +0900

    Create endpoints to create attendance

--- backend/src/modules/attendances/attendances.controller.ts
+++ backend/src/modules/attendances/attendances.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { Attendance } from 'src/entities/Attendance.entity';
+import { RequestWithUserID } from 'src/utils/type';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { AttendancesService } from './attendances.service';
+import { CreateAttendanceDto } from './Dto/CreateAttendance.Dto';
 import { getAttendancesQueryDto } from './Dto/GetAttendancesQuery.Dto';
 
 @Controller('attendances')
@@ -14,5 +25,14 @@ export class AttendancesController {
     @Query() query: getAttendancesQueryDto,
   ): Promise<Attendance[]> {
     return this.attendancesService.getAttendances(query);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  create(
+    @Req() req: RequestWithUserID,
+    @Body() newData: CreateAttendanceDto,
+  ): Promise<Attendance> {
+    return this.attendancesService.create(req.user.userID, newData);
   }
 }
--- backend/src/modules/attendances/attendances.service.ts
+++ backend/src/modules/attendances/attendances.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Attendance } from 'src/entities/Attendance.entity';
+import { User } from 'src/entities/User.entity';
 import { Repository } from 'typeorm';
+import { CreateAttendanceDto } from './Dto/CreateAttendance.Dto';
 import { getAttendancesQueryDto } from './Dto/GetAttendancesQuery.Dto';
 
 @Injectable()
@@ -9,6 +11,9 @@ export class AttendancesService {
   constructor(
     @InjectRepository(Attendance)
     private attendancesRepository: Repository<Attendance>,
+
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
   ) {}
 
   async getAttendances(query: getAttendancesQueryDto): Promise<Attendance[]> {
@@ -22,5 +27,13 @@ export class AttendancesService {
       .andWhere('u.id = :id', { id: query.user_id })
       .getMany();
     return attendances;
+  }
+
+  async create(
+    userID: number,
+    newData: CreateAttendanceDto,
+  ): Promise<Attendance> {
+    const requestUser = await this.usersRepository.findOne(userID);
+    return this.attendancesRepository.save({ ...newData, user: requestUser });
   }
 }
