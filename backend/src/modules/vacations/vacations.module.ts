2f88f7ebdc3d390f16555dedbe1866e662f1d2ce
k-tkm <kjohlkjou89@gmail.com>
2022-06-07 09:14:29 +0900

    Resolve dependency errors

--- backend/src/modules/vacations/vacations.module.ts
+++ backend/src/modules/vacations/vacations.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Vacation } from 'src/entities/Vacation.entity';
 import { VacationsController } from './vacations.controller';
 import { VacationsService } from './vacations.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Vacation])],
   controllers: [VacationsController],
-  providers: [VacationsService]
+  providers: [VacationsService],
 })
 export class VacationsModule {}
