8d28dfece8221a87648a830409f0a10dd9d13df6
k-tkm <kjohlkjou89@gmail.com>
2022-06-08 09:44:38 +0900

    Fixed a bug that prevented access to endpoints.

--- backend/src/modules/attendances/attendances.module.ts
+++ backend/src/modules/attendances/attendances.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Attendance } from 'src/entities/Attendance.entity';
+import { User } from 'src/entities/User.entity';
 import { AttendancesController } from './attendances.controller';
 import { AttendancesService } from './attendances.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Attendance])],
+  imports: [TypeOrmModule.forFeature([Attendance, User])],
   controllers: [AttendancesController],
   providers: [AttendancesService],
 })
