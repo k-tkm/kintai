04f6a1e48ff84a5578fbfb7635bfb222daec7848
k-tkm <kjohlkjou89@gmail.com>
2022-06-10 09:50:30 +0900

    yarn add chakra-ui

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </QueryClientProvider>
   );
 }
