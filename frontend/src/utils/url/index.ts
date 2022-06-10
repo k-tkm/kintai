6dbbb6b198abc274adcab39d74d37ab087e227a1
k-tkm <kjohlkjou89@gmail.com>
2022-06-10 09:28:55 +0900

    Solve localStorage is not defined Error

--- frontend/src/utils/url/index.ts
+++ frontend/src/utils/url/index.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
 
-const token = localStorage.getItem("accessToken");
+const getToken = () => {
+  if (typeof window !== "undefined") {
+    return localStorage.getItem("accessToken") || "";
+  }
+  return "";
+};
+
 export const axiosInstance = axios.create({
   baseURL: "http://localhost:9000",
   headers: {
-    Authorization: `Bearer ${token}`,
+    Authorization: `Bearer ${getToken()}`,
     "Content-Type": "application/json",
   },
 });
