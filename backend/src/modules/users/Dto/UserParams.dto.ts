ea3c1ed8ce5410d911f8a8199e7d8a4835700cc6
k-tkm <kjohlkjou89@gmail.com>
2022-06-16 09:51:34 +0900

    Fixed wording of error messages during path parameter validation

--- backend/src/modules/users/Dto/UserParams.dto.ts
+++ backend/src/modules/users/Dto/UserParams.dto.ts
@@ -1,6 +1,6 @@
 import { IsNumberString } from 'class-validator';
 
 export class UserParamsDto {
-  @IsNumberString({}, { message: 'パラメータ値には数値を入力してください。' })
+  @IsNumberString({}, { message: 'パスパラメータには数字を使用してください。' })
   id: string;
 }
