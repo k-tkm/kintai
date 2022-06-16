6cff0c8f9784b75341e0b078a962c61ccfee91cb
k-tkm <kjohlkjou89@gmail.com>
2022-06-16 10:54:23 +0900

    Corrected the wording of error messages when validating

--- backend/src/modules/departments/Dto/CreateDepartmentDto.ts
+++ backend/src/modules/departments/Dto/CreateDepartmentDto.ts
@@ -2,11 +2,11 @@ import { IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { User } from 'src/entities/User.entity';
 
 export class CreateDepartmentDto {
-  @IsString({ message: '部署名には文字列を入力してください。' })
+  @IsString({ message: '部署名には文字列を使用してください。' })
   @IsNotEmpty({ message: '部署名は入力必須です。' })
   name: string;
 
-  @IsArray({ message: 'usersの値は配列で入力してください。' })
+  @IsArray({ message: 'usersには配列を使用してください。' })
   @IsOptional()
   users?: User[];
 }
--- backend/src/modules/departments/Dto/UpdateDepartmentDto.ts
+++ backend/src/modules/departments/Dto/UpdateDepartmentDto.ts
@@ -2,12 +2,12 @@ import { IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { User } from 'src/entities/User.entity';
 
 export class UpdateDepartmentDto {
-  @IsString({ message: '部署名には文字列を入力してください。' })
+  @IsString({ message: '部署名には文字列を使用してください。' })
   @IsNotEmpty({ message: '部署名は入力必須です。' })
   @IsOptional()
   name?: string;
 
-  @IsArray({ message: 'usersの値は配列で入力してください。' })
+  @IsArray({ message: 'usersには配列を使用してください。' })
   @IsOptional()
   users?: User[];
 }
