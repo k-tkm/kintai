ddd1b76a5f8986262ff0af85e43564eadb74252a
k-tkm <kjohlkjou89@gmail.com>
2023-03-08 22:55:25 +0900

    Delete unneccesary query

--- backend/src/migrations/1678283567046-add-colomn-password-in-users-table.ts
+++ backend/src/migrations/1678283567046-add-colomn-password-in-users-table.ts
@@ -12,30 +12,6 @@ export class addColomnPasswordInUsersTable1678283567046
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "attendances" DROP CONSTRAINT "FK_90bec1a9a7766a4707d43d7af80"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "departments" DROP CONSTRAINT "FK_541e3d07c93baa9cc42b149a5fb"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "user_departments" DROP CONSTRAINT "FK_b59bda425551ddbc1b486f1d82e"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "users" DROP CONSTRAINT "FK_7ae6334059289559722437bcc1c"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "vacations" DROP CONSTRAINT "FK_a384ba5002ef45bd00242fc7f1f"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "attendances" DROP COLUMN "company_id"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "departments" DROP COLUMN "company_id"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "user_departments" DROP COLUMN "company_id"`,
-    );
     await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "password"`);
   }
 }
